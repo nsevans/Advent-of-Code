f7e39b7f0b023cde51df8b46c0cfb2ae6e2a0c30
Nicholas Evans <nicholas.evans97@outlook.com>
2024-12-13 15:33:48 -0400

    Transferred puzzle #2 for 2023 to new structure

---
+++ src/AdventOfCode/Puzzles/2023/Puzzle_02/Part_01.cs
@@ -0,0 +1,66 @@
+using System;
+using System.Collections.Generic;
+
+namespace AdventOfCode.Puzzles.Year_2023.Puzzle_02;
+
+public class Part_01 : Puzzle_02
+{
+	
+	private const int _redLimit = 12;
+	private const int _greenLimit = 13;
+	private const int _blueLimit = 14;
+	private const int _overallLimit = 14;
+
+	private List<string> _preparedInput;
+
+    public override void PrepareData(List<string> input)
+    {
+		_preparedInput = input;
+    }
+
+    public override void Solve()
+    {
+		var result = CalculateValidGames(_preparedInput);
+		Console.WriteLine($"Sum of Game IDs that match criteria: {result}");
+    }
+
+	private static int CalculateValidGames(IEnumerable<string> input)
+	{
+		var sum = 0;
+		foreach (var line in input)
+		{
+			var parsedLine = line.Split(' ');
+
+			// Each line starts with "Game ##:", so we can safely assume that
+			// the id is at index 1 after splitting and removing ':' from the end
+			var gameId = int.Parse(parsedLine[1][..^1]);
+
+			var isValidGame = true;
+			for (int i = 2; i < parsedLine.Length; i += 2)
+			{
+				var number = int.Parse(parsedLine[i]);
+				var color = parsedLine[i + 1];
+
+				isValidGame =
+					// If any of the inputs exceed the overall limit we can save at least
+					// a calculation since it doesn't matter what colour it's for
+					number <= _overallLimit
+					// Validate the first letter of each color since they're unique
+					&& (
+						(color[0] == 'r' && number <= _redLimit)
+						|| (color[0] == 'g' && number <= _greenLimit)
+						|| (color[0] == 'b' && number <= _blueLimit)
+					);
+
+				// As soon as it's not a valid game stop iterating
+				if (!isValidGame)
+					break;
+			}
+
+			if (isValidGame)
+				sum += gameId;
+		}
+
+		return sum;
+	}
+}
---
+++ src/AdventOfCode/Puzzles/2023/Puzzle_02/Part_02.cs
@@ -0,0 +1,48 @@
+using System;
+using System.Collections.Generic;
+
+namespace AdventOfCode.Puzzles.Year_2023.Puzzle_02;
+
+public class Part_02 : Puzzle_02
+{
+	private List<string> _preparedInput;
+    public override void PrepareData(List<string> input)
+    {
+		_preparedInput = input;
+    }
+
+    public override void Solve()
+    {
+		var result = CalculateSumOfPowerOfEachGame(_preparedInput);
+		Console.WriteLine($"Sum of the powers of the fewest cubes of each color per game: {result}");
+    }
+
+	private static long CalculateSumOfPowerOfEachGame(IEnumerable<string> input)
+	{
+		var sum = 0L;
+		foreach (var line in input)
+		{
+			var minRed = int.MinValue;
+			var minGreen = int.MinValue;
+			var minBlue = int.MinValue;
+
+			var parsedLine = line.Split(' ');
+			for (int i = 2; i < parsedLine.Length; i += 2)
+			{
+				var number = int.Parse(parsedLine[i]);
+				var color = parsedLine[i + 1];
+
+				if (color[0] == 'r')
+					minRed = number > minRed ? number : minRed;
+				else if (color[0] == 'g')
+					minGreen = number > minGreen ? number : minGreen;
+				else if (color[0] == 'b')
+					minBlue = number > minBlue ? number : minBlue;
+			}
+
+			sum += minRed * minGreen * minBlue;
+		}
+
+		return sum;
+	}
+}

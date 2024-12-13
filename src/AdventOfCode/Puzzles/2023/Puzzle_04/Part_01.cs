bdce5cdceba39d512990f0336f89512f5cb38c9f
Nicholas Evans <nicholas.evans97@outlook.com>
2024-12-13 15:34:46 -0400

    Transferred puzzle #4 for 2023 to new structure

---
+++ src/AdventOfCode/Puzzles/2023/Puzzle_04/Part_01.cs
@@ -0,0 +1,46 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace AdventOfCode.Puzzles.Year_2023.Puzzle_04;
+
+public class Part_01 : Puzzle_04
+{
+	private List<string> _preparedInput;
+    public override void PrepareData(List<string> input)
+    {
+		_preparedInput = input;
+    }
+
+    public override void Solve()
+    {
+		var result = CalculatePointsForMatchingNumbers(_preparedInput);
+		Console.WriteLine($"Sum of points for matching number: {result}");
+    }
+
+	private static int CalculatePointsForMatchingNumbers(List<string> input)
+	{
+		var sum = 0;
+
+		foreach(var line in input)
+		{
+			var split = line.Split('|');
+			// Get left side of card, removing first two entries "Card" and "##:"
+			var references = split[0].Split(" ", StringSplitOptions.RemoveEmptyEntries)[2..].Select(x => int.Parse(x));
+			var values = split[1].Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(x => int.Parse(x));
+			
+			var cardPoints = 0;
+			foreach(var value in values)
+			{
+				if (references.Contains(value))
+				{
+					// Set to 1 on first set, otherwise multiple result by 2
+					cardPoints = cardPoints == 0 ? 1 : cardPoints * 2;
+				}
+			}
+			sum += cardPoints;
+		}
+
+		return sum;
+	}
+}
---
+++ src/AdventOfCode/Puzzles/2023/Puzzle_04/Part_02.cs
@@ -0,0 +1,81 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace AdventOfCode.Puzzles.Year_2023.Puzzle_04;
+
+public class Part_02 : Puzzle_04
+{
+	private Dictionary<int, Card> _cardDictionary;
+	private List<int> _cardsToScratch;
+	private int _lastCardId;
+
+	private record Card(int Id, IList<int> ReferenceValues, IList<int> CardValues);
+
+    public override void PrepareData(List<string> input)
+    {
+		// Reference dictionary of all available cards
+		_cardDictionary = ConstructCardDictionary(input);
+		// Maintained list of cards that can be scratched, new cards are added to the end of the list
+		_cardsToScratch = _cardDictionary.Keys.ToList();
+		// Id of last card id, used to ensure no card ids greater than it are added
+		_lastCardId = _cardsToScratch.OrderBy(x => x).Last();
+    }
+	
+	private static Dictionary<int, Card> ConstructCardDictionary(IEnumerable<string> input)
+	{
+		var dictionary = new Dictionary<int, Card>();
+		foreach(var line in input)
+		{
+			var split = line.Split('|');
+			// Parse card id from line, removing trailing ':'
+			var cardId = int.Parse(split[0].Split(" ", StringSplitOptions.RemoveEmptyEntries)[1][..^1]);
+			// Get left side of card, removing first two entries "Card" and "##:"
+			var references = split[0].Split(" ", StringSplitOptions.RemoveEmptyEntries)[2..]
+				.Select(x => int.Parse(x))
+				.ToList();
+			var values = split[1].Split(" ", StringSplitOptions.RemoveEmptyEntries)
+				.Select(x => int.Parse(x))
+				.ToList();
+
+			var card = new Card(cardId, references, values);
+			dictionary.Add(cardId, card);
+		}
+
+		return dictionary;
+	}
+
+    public override void Solve()
+    {
+		var result = CalculateNumberOfInstancesOfWinningCards(_cardDictionary, _cardsToScratch, _lastCardId);
+		Console.WriteLine($"Sum of points for matching number: {result}");
+    }
+
+	private static int CalculateNumberOfInstancesOfWinningCards(Dictionary<int, Card> cardDictionary, List<int> cardsToScratch, int lastCardId)
+	{	
+		var numberOfCardsScratched = 0;
+
+		for (int i = 0; i < cardsToScratch.Count; i++)
+		{
+			numberOfCardsScratched++;
+			// Get current card from dictionary
+			var scratchedCard = cardDictionary[cardsToScratch[i]];
+
+			var numberOfMatches = 0;
+			foreach(var value in scratchedCard.CardValues)
+			{
+				if (scratchedCard.ReferenceValues.Contains(value))
+				{
+					numberOfMatches++;
+					var cardWonId = scratchedCard.Id + numberOfMatches;
+					// Ensure card id being added isn't greater than the largest available card id
+					if (cardWonId <= lastCardId)
+					{
+						cardsToScratch.Add(cardWonId);
+					}
+				}
+			}
+		}
+		return numberOfCardsScratched;
+	}
+}

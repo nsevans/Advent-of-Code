2012d72e4a71e73508c29b3cdbdf0ee93fbddced
Nicholas Evans <nicholas.evans97@outlook.com>
2024-12-15 17:36:52 -0400

    Add new list extension methods

--- src/AdventOfCode.Common/Extensions/ListExtensions.cs
+++ src/AdventOfCode.Common/Extensions/ListExtensions.cs
@@ -20,7 +20,7 @@ public static class ListExtensions
 	/// <param name="x"></param>
 	/// <param name="y"></param>
 	/// <returns></returns>
-	public static bool IsInBounds(this IList<IList<object>> map, int x, int y)
+	public static bool IsInBounds<T>(this List<List<T>> map, int x, int y)
 	{
 		if (x < 0 || y < 0)
 			return false;
@@ -41,5 +41,100 @@ public static class ListExtensions
 	public static void SwapValues<T>(this IList<T> list, int index1, int index2)
 	{
 		(list[index1], list[index2]) = (list[index2], list[index1]);
+	}
+
+	/// <summary>
+	/// Searches the given value and returns the index of the first occurrence within the 2 dimensional list.
+	/// </summary>
+	/// <typeparam name="T"></typeparam>
+	/// <param name="list"></param>
+	/// <param name="value"></param>
+	/// <returns>Returns the index of the first occurrence of the item if found, otherwise (-1, -1)</returns>
+	public static (int x, int y) IndexOf<T>(this List<List<T>> list, T value)
+	{
+		if (list.Where(x => x.Contains(value)).Count() == 0)
+			return (-1, -1);
+
+		var row = list.Where(x => x.Contains(value)).First();
+		var y = list.IndexOf(row);
+		var x = row.IndexOf(value);
+
+		return (x, y);
+	}
+
+	/// <summary>
+	/// Searches the given value and returns all indexes within the 2 dimensional list.
+	/// </summary>
+	/// <typeparam name="T"></typeparam>
+	/// <param name="list"></param>
+	/// <param name="value"></param>
+	/// <returns>Returns a list of indexes of the item if found, otherwise an empt list</returns>
+	public static List<(int x, int y)> IndexesOf<T>(this List<List<T>> list, T value)
+	{
+		var indexes = new List<(int x, int y)>();
+
+		foreach (var (row, y) in list.Select((r, i) => (r, i)))
+		{
+			foreach (var (item, x) in row.Select((i, j) => (i, j)))
+			{
+				if (item.Equals(value))
+					indexes.Add((x, y));
+			}
+		}
+
+		return indexes;
+	}
+
+	/// <summary>
+	/// Get a value from a specified index of a 2D list at a given position, as long as the position is valid
+	/// </summary>
+	/// <typeparam name="T"></typeparam>
+	/// <param name="list"></param>
+	/// <param name="position"></param>
+	/// <returns>The value at the given index, default if the index is invalid</returns>
+	public static T GetFromIndex<T>(this List<List<T>> list, (int x, int y) position) => list.GetFromIndex(position.x, position.y);
+
+	/// <summary>
+	/// Get a value from a specified index of a 2D list at a given position, as long as the position is valid
+	/// </summary>
+	/// <typeparam name="T"></typeparam>
+	/// <param name="list"></param>
+	/// <param name="x"></param>
+	/// <param name="y"></param>
+	/// <returns>The value at the given index, default if the index is invalid</returns>
+	public static T GetFromIndex<T>(this List<List<T>> list, int x, int y)
+	{
+		if (!list.IsInBounds(x, y))
+			return default;
+
+		return list[y][x]; 
+	}
+
+	/// <summary>
+	/// Set a given value in a 2D list at a given position, as long as the position is valid 
+	/// </summary>
+	/// <typeparam name="T"></typeparam>
+	/// <param name="list"></param>
+	/// <param name="position"></param>
+	/// <param name="value"></param>
+	/// <returns>True if set successfully, false otherwise</returns>
+	public static bool SetFromIndex<T>(this List<List<T>> list, (int x, int y) position, T value) => list.SetFromIndex(position.x, position.y, value);
+
+	/// <summary>
+	/// Set a given value in a 2D list at a given position, as long as the position is valid 
+	/// </summary>
+	/// <typeparam name="T"></typeparam>
+	/// <param name="list"></param>
+	/// <param name="x"></param>
+	/// <param name="y"></param>
+	/// <param name="value"></param>
+	/// <returns>True if set successfully, false otherwise</returns>
+	public static bool SetFromIndex<T>(this List<List<T>> list, int x, int y, T value)
+	{
+		if (!list.IsInBounds(x, y))
+			return false;
+
+		list[y][x] = value;
+		return true;
 	}
 }

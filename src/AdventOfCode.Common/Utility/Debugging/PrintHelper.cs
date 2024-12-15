fa3102fada331c213c919300881e389d3aa2a5d1
Nicholas Evans <nicholas.evans97@outlook.com>
2024-12-15 17:37:46 -0400

    Create a print helper with a method to better visualize lists

---
+++ src/AdventOfCode.Common/Utility/Debugging/PrintHelper.cs
@@ -0,0 +1,36 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace AdventOfCode.Common.Utility.Debugging;
+
+public static class PrintHelper
+{
+	public static void PrintMap<T>(List<List<T>> map, List<T> highlightValues = null, List<(int x, int y)> highlightIndexes = null, char highlighter = '_', bool padItems = true)
+	{
+		highlightValues ??= [];
+		highlightIndexes ??= [];
+
+		foreach (var (row, yIndex) in map.Select((r, y) => (r, y)))
+		{
+			foreach (var (item, xIndex) in row.Select((i, x) => (i, x)))
+			{
+				if (highlightIndexes.Contains((xIndex, yIndex)) || highlightValues.Contains(item))
+				{
+					if (padItems)
+						Console.Write($" {highlighter}{item}{highlighter} ");
+					else
+						Console.Write($"{highlighter}{item}{highlighter}");
+				}
+				else
+				{
+					if (padItems)
+						Console.Write($"  {item}  ");
+					else
+						Console.Write($"{item}");
+				}
+			}
+			Console.WriteLine();
+		}
+	}
+}
